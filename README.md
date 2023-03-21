bd72d6477d3f30844a0ae98105ef70af8b2134e1
Julio César <llulioscesar@gmail.com>
2023-03-20 23:17:03 -0500

    Improved password package with constant-time comparison, error handli… (#2)
    …ng, and added methods for retrieving and checking the hashed password.
    Also updated the NOTICE file to include information about the
    third-party dependencies used.

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ This is a collection of utility functions for Go.
 ```bash
 go get github.com/developersgotech/go-util
 ```
+
+## Licencia
+
+Este proyecto se distribuye bajo la Licencia Apache 2.0. Ver el archivo [LICENSE](LICENSE) para obtener más detalles.
+
+## Atribuciones
+
+Este software utiliza la biblioteca 'github.com/google/uuid', bajo la Licencia BSD de 3 cláusulas. Ver el archivo [NOTICE](NOTICE) para obtener más detalles.****
---
+++ password/password.go
@@ -0,0 +1,51 @@
+package password
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+type (
+	// Password is a struct that holds the hashed password.
+	Password struct {
+		hashedPassword string
+	}
+)
+
+// NewPassword returns a noop password.
+func NoopPassword() Password {
+	return Password{
+		hashedPassword: "",
+	}
+}
+
+// HashPassword returns a new password from plain password text.
+func HashPassword(password string, cost int) (Password, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
+		return NoopPassword(), err
+	}
+	return Password{
+		hashedPassword: string(hashed),
+	}, nil
+}
+
+// FromHash creates a Password from a hashed password and its corresponding plain-text password.
+// It returns an error if the hashed password does not match the plain-text password.
+func FromHash(hashedPassword string, password string) (Password, error) {
+	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)); err != nil {
+		return NoopPassword(), ErrPasswordNotMatch
+	}
+	return Password{
+		hashedPassword: hashedPassword,
+	}, nil
+}
+
+// IsNil returns true if the password is nil or empty.
+func (p *Password) IsNil() bool {
+	return p.hashedPassword == ""
+}
+
+// String returns the hashed password.
+func (p *Password) String() string {
+	return p.hashedPassword
+}
