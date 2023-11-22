94caec3738a571a67dc757c7b51c1aee423287e3
Julio Caicedo <llulioscesar@gmail.com>
2023-11-22 08:24:53 -0500

    feat: Add function that allows validating any regular expression

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.20
 require (
 	github.com/google/uuid v1.4.0
 	github.com/startcodextech/goerrors v0.0.5
-	golang.org/x/crypto v0.14.0
+	golang.org/x/crypto v0.15.0
 )
--- validator/email.go
+++ validator/email.go
@@ -1,13 +1,13 @@
 package validator
 
-import "regexp"
+const (
+	EmailRegexp = `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
+)
 
 type Email string
 
 func (e Email) IsValid() bool {
-	expresionRegular := `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
-	valido, err := regexp.MatchString(expresionRegular, string(e))
-	return valido && err == nil
+	return Regexp(string(e), EmailRegexp)
 }
 
 func (e Email) String() string {
